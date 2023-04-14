ace6a0ff041e0cca91664b5aba9f23daaef593ce
Waldemar Kmiecik <waldemar.kmiecik@microsoft.wsei.edu.pl>
2023-04-14 19:36:11 +0200

    Added GameData and GameManager singleton

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType<GameManager>();
+            }
+
+            return _instance;
+        }
+    }
+
+    void Awake()
+    {
+        _instance = this;
+    }
+
+    public GameData gameData;
+}
